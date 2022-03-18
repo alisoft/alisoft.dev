7181189add5ca8ab90a75582bb99e681d301ee71
see <upcwangying@126.com>
2022-03-18 05:42:31 +0000

    deploy: 416658cfbead758de447cd064d5a6c28b1338e48

--- _nuxt/static/1647520309/zh/examples/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647520309",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fzh\u002Fexamples",config:{apiURL:b,plausible:{domain:b},recaptcha:{version:3,siteKey:b},_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},docusDbHash:"f40f629b"},globalRefs:{}}}(null,void 0));
